14091dcd5431fd475c1e74c5efd1971d71c93ae9
Justus Perlwitz <mail@justusperlwitz.com>
2016-10-08 22:38:33 +0200

    Chapter 4

--- main.cpp
+++ main.cpp
@@ -2,7 +2,19 @@
 #include "vec3.h"
 #include <iostream>
 
+bool hit_sphere(const vec3 &center, float radius, const ray &r) {
+  vec3 oc = r.origin() - center;
+  auto a = r.direction().dot(r.direction());
+  auto b = oc.dot(r.direction()) * 2.0;
+  auto c = oc.dot(oc) - radius * radius;
+  auto discriminant = b * b - 4 * a * c;
+  return (discriminant > 0);
+}
+
 vec3 color(const ray &r) {
+  if (hit_sphere(vec3(0, 0, -1), 0.5, r)) {
+    return vec3(1, 0, 0);
+  }
   auto unit_direction = r.direction().unit_vector();
   auto t = 0.5 * (unit_direction.y() + 1.0);
   return vec3(1.0, 1.0, 1.0) * (1.0 - t) + vec3(0.5, 0.7, 1.0) * t;

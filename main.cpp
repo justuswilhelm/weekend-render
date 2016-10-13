7f187a835e982187bbc011bd0d519cc126d06bb8
Justus Perlwitz <mail@justusperlwitz.com>
2016-10-13 22:24:08 +0200

    Fix formatting

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
+#include "camera.h"
 #include "float.h"
 #include "hitable_list.h"
 #include "ray.h"
 #include "sphere.h"
-#include <random>
 #include "vec3.h"
-#include "camera.h"
 #include <iostream>
+#include <random>
 
 vec3 color(const ray &r, hitable *world) {
   hit_record rec;
@@ -28,14 +28,13 @@ int main() {
 
   camera cam;
 
-  auto *world = new hitable_list({
-      new sphere(vec3(0, 0, -1), 0.5),
-      new sphere(vec3(0, -100.5, -1), 100)});
+  auto *world = new hitable_list(
+      {new sphere(vec3(0, 0, -1), 0.5), new sphere(vec3(0, -100.5, -1), 100)});
 
   for (auto j = ny - 1; j >= 0; j--) {
     for (auto i = 0; i < nx; i++) {
       vec3 col(0, 0, 0);
-      for (int s=0; s < ns; s++) {
+      for (int s = 0; s < ns; s++) {
         auto u = float(i + drand48()) / float(nx);
         auto v = float(j + drand48()) / float(ny);
         auto r = cam.get_ray(u, v);

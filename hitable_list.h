bebbe5e4f0f3af349a9c1de6181b08aa3b25cad0
Justus Perlwitz <mail@justusperlwitz.com>
2016-10-13 22:29:06 +0200

    Use list initialization

--- hitable_list.h
+++ hitable_list.h
@@ -5,7 +5,7 @@
 class hitable_list : public hitable {
 public:
   hitable_list() {}
-  hitable_list(std::vector<hitable *> l) { list = l; }
+  hitable_list(std::vector<hitable *> l) : list(l) {}
   virtual bool hit(const ray &r, float tmin, float tmax, hit_record &rec) const;
   std::vector<hitable *> list;
 };

dfd29c3d8bf6b8e847e7aca29acb25b7f7ebc648
Justus Perlwitz <mail@justusperlwitz.com>
2016-10-13 23:55:32 +0200

    Chapter 7

--- main.cpp
+++ main.cpp
@@ -8,10 +8,19 @@
 #include <memory>
 #include <random>
 
+vec3 random_in_unit_sphere() {
+  vec3 p;
+  do {
+    p = vec3(drand48(), drand48(), drand48()) * 2.0 - vec3(1, 1, 1);
+  } while (p.squared_length() >= 1.0);
+  return p;
+}
+
 vec3 color(const ray &r, hitable &world) {
   hit_record rec;
-  if (world.hit(r, 0.0, FLT_MAX, rec)) {
-    return vec3(rec.normal.x + 1, rec.normal.y + 1, rec.normal.z + 1) * 0.5;
+  if (world.hit(r, 0.001, FLT_MAX, rec)) {
+    vec3 target = rec.p + rec.normal + random_in_unit_sphere();
+    return color(ray(rec.p, target - rec.p), world) * 0.5;
   } else {
     auto unit_direction = r.direction().unit_vector();
     auto t = 0.5 * (unit_direction.y + 1.0);
@@ -29,7 +38,7 @@ int main() {
   camera cam;
 
   auto a = std::make_shared<sphere>(sphere(vec3(0, 0, -1), 0.5));
-  auto b = std::make_shared<sphere>(sphere(vec3(0, -100.5, -1), 100));
+  auto b = std::make_shared<sphere>(sphere(vec3(0, 100.5, -1), 100));
   hitable_list world({a, b});
 
   for (auto j = 0; j < ny; j++) {
@@ -42,6 +51,7 @@ int main() {
         col = col + color(r, world);
       }
       col = col / float(ns);
+      col = vec3(sqrt(col.x), sqrt(col.y), sqrt(col.z));
 
       auto ir = int(255 * col.x);
       auto ig = int(255 * col.y);
